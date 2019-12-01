d766231e5442919d3c6884781df504b04789d699
Alexey Gumirov <a.gumirov@telekom.com>
2019-12-01 21:26:35 +0100

    first beta is created.

--- my_env_setup.sh
+++ my_env_setup.sh
@@ -16,73 +16,218 @@ SETUP_LOG="$HOME/my_env_setup.log"
 timestamp=$(date +%T.%N)
 echo -ne "\r$timestamp: Setup of environment is starting.\n" > "$SETUP_LOG"
 
-# install NeoVIM
+### {
+# Begin - Repo update and upgrade 
 
-TOPIC="NeoVIM installation:"
+TOPIC="APT Repository:"
 timestamp=$(date +%T.%N)
-echo -ne "$TOPIC --- Adding NeoVIM repository --- : $timestamp\n" | tee -a "$SETUP_LOG"
-sudo add-apt-repository ppa:neovim-ppa/unstable -y | tee -a "$SETUP_LOG"
+echo -ne "$TOPIC --- Update and upgrade --- : $timestamp\n" | tee -a "$SETUP_LOG"
 sudo apt-get update -y | tee -a "$SETUP_LOG" 
-echo -ne "$TOPIC --- Installing NeoVIM --- : $timestamp\n" | tee -a "$SETUP_LOG"
+sudo apt-get upgrade -y | tee -a "$SETUP_LOG" 
+timestamp=$(date +%T.%N)
+echo -ne "$TOPIC --- Update and upgrade complete --- : $timestamp\n" | tee -a "$SETUP_LOG"
+
+# End - Repo update and upgrade
+### }
+
+
+### {
+# Begin - Install NeoVIM
+
+TOPIC="NeoVIM:"
+NEOVIM_PLUG_DIR="$HOME/.local/share/nvim/site/autoload"
+NEOVIM_PLUGINS_DIR="$HOME/.local/share/nvim/plugged"
+NEOVIM_CONFIG_DIR="$HOME/.config/nvim"
+
+timestamp=$(date +%T.%N)
+echo -ne "$TOPIC --- Adding repository --- : $timestamp\n" | tee -a "$SETUP_LOG"
+sudo add-apt-repository ppa:neovim-ppa/unstable -y | tee -a "$SETUP_LOG"
+
+timestamp=$(date +%T.%N)
+echo -ne "$TOPIC --- Installing --- : $timestamp\n" | tee -a "$SETUP_LOG"
 sudo apt-get install neovim -y | tee -a "$SETUP_LOG"
-timestamp=$(date +%T.%N)
-echo -ne "\n\r$TOPIC --- Installation complete  ---: $timestamp\n" | tee -a "$SETUP_LOG"
 
 timestamp=$(date +%T.%N)
-echo -ne "\n\r$TOPIC --- Installing VIM plug-ins manager ---: $timestamp\n" | tee -a "$SETUP_LOG"
-
-# install NeoVIM plugin manager
-
-TOPIC="NeoVIM Plug in manager:"
-VIM_PLUG_DIR="$HOME/.local/share/nvim/site/autoload/"
+echo -ne "\n\r$TOPIC --- Installation complete  --- : $timestamp\n" | tee -a "$SETUP_LOG"
 
 timestamp=$(date +%T.%N)
-echo -ne "\n\r$TOPIC --- Download vim.plug ---: $timestamp\n" | tee -a "$SETUP_LOG"
-curl -fLo "$VIM_PLUG_DIR"plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-echo -ne "\n\r$TOPIC --- Downloading of vim.plug is complete ---: $timestamp\n" | tee -a "$SETUP_LOG"
+echo -ne "\n\r$TOPIC --- Download vim.plug --- : $timestamp\n" | tee -a "$SETUP_LOG"
+curl -fLo "$NEOVIM_PLUG_DIR"/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+timestamp=$(date +%T.%N)
+echo -ne "\n\r$TOPIC --- Downloading of vim.plug complete --- : $timestamp\n" | tee -a "$SETUP_LOG"
 
-NEOVIM_PLUGINS_DIR="$HOME/.local/share/nvim/plugged/"
-if [[ ! -d $NEOVIM_PLUGINS_DIR ]]
+if [[ ! -d $NEOVIM_CONFIG_DIR ]]
 then
-	mkdir -p "$NEOVIM_PLUGINS_DIR"
+	mkdir -p "$NEOVIM_CONFIG_DIR" | tee -a "$SETUP_LOG"
+fi
+timestamp=$(date +%T.%N)
+echo -ne "\n\r$TOPIC --- Copy init.vim --- : $timestamp\n" | tee -a "$SETUP_LOG"
+cp "$SETUP_DIR"/configs/neovim/init.vim "$NEOVIM_CONFIG_DIR"/ | tee -a "$SETUP_LOG"
+
+if [[ ! -d $NEOVIM_CONFIG_DIR/colors ]]
+then
+	mkdir -p "$NEOVIM_CONFIG_DIR"/colors | tee -a "$SETUP_LOG"
 fi
 
-# install python3
-# install python3-pip
+timestamp=$(date +%T.%N)
+echo -ne "\n\r$TOPIC --- Copy color scheme --- : $timestamp\n" | tee -a "$SETUP_LOG"
+cp "$SETUP_DIR"/configs/vim_theme/PaperColor.vim "$NEOVIM_CONFIG_DIR"/colors/ | tee -a "$SETUP_LOG"
 
-sudo apt-get install -y python3-pip
+if [[ ! -d $NEOVIM_PLUGINS_DIR ]]
+then
+	mkdir -p "$NEOVIM_PLUGINS_DIR" | tee -a "$SETUP_LOG"
+fi
 
-# install neovim-remote
+timestamp=$(date +%T.%N)
+echo -ne "\n\r$TOPIC --- Installing \"neovim-remote\" --- : $timestamp\n" | tee -a "$SETUP_LOG"
+sudo apt-get install python3-pip -y | tee -a "$SETUP_LOG"
+pip3 install -y neovim-remote | tee -a "$SETUP_LOG"
 
-pip3 install neovim-remote
+timestamp=$(date +%T.%N)
+echo -ne "\n\r$TOPIC --- Intstallation of \"neovim-remote\" complete --- : $timestamp\n" | tee -a "$SETUP_LOG"
 
-# install fzf
+echo -ne "\n\r$TOPIC --- Intstallation complete --- : $timestamp\n" | tee -a "$SETUP_LOG"
 
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-~/.fzf/install
-
-# setup bash environment
-
-
-# create .vimrc and make basic config
-
-
-
-# install vim as main system editor
-
-# install tmux
-
-sudo apt-get install -y tmux
-
-# install tmux configuration
+# End - Install NeoVIM
+### }
 
 
-# Install tmux plug manager
+### {
+# Begin - Install FZF
 
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+TOPIC="FZF:"
 
-# Install powerline for bash
+timestamp=$(date +%T.%N)
+echo -ne "\n\r$TOPIC --- Installting --- : $timestamp\n" | tee -a "$SETUP_LOG"
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf | tee -a "$SETUP_LOG"
+~/.fzf/install | tee -a "$SETUP_LOG"
 
-sudo apt-get install -y powerline
+timestamp=$(date +%T.%N)
+echo -ne "\n\r$TOPIC --- Installation complete --- : $timestamp\n" | tee -a "$SETUP_LOG"
 
-# Clone config from the repository and copy it
+# End - Install FZF
+### }
+
+
+### {
+# Begin - Install VIM
+
+TOPIC="VIM:"
+VIM_CONFIG_DIR="$HOME/.vim"
+
+timestamp=$(date +%T.%N)
+echo -ne "$TOPIC --- Removing \"vim-tiny\" --- : $timestamp\n" | tee -a "$SETUP_LOG"
+sudo apt-get remove -y vim-tiny | tee -a "$SETUP_LOG"
+
+timestamp=$(date +%T.%N)
+echo -ne "$TOPIC --- Installing --- : $timestamp\n" | tee -a "$SETUP_LOG"
+sudo apt-get install vim -y | tee -a "$SETUP_LOG"
+timestamp=$(date +%T.%N)
+echo -ne "\n\r$TOPIC --- Installation complete  --- : $timestamp\n" | tee -a "$SETUP_LOG"
+
+timestamp=$(date +%T.%N)
+echo -ne "\n\r$TOPIC --- Download vim.plug --- : $timestamp\n" | tee -a "$SETUP_LOG"
+curl -fLo "$VIM_CONFIG_DIR"/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+timestamp=$(date +%T.%N)
+echo -ne "\n\r$TOPIC --- Downloading of vim.plug complete --- : $timestamp\n" | tee -a "$SETUP_LOG"
+
+timestamp=$(date +%T.%N)
+echo -ne "\n\r$TOPIC --- Copy .vimrc --- : $timestamp\n" | tee -a "$SETUP_LOG"
+cp "$SETUP_DIR"/configs/vim/.vimrc "$HOME"/ | tee -a "$SETUP_LOG"
+
+if [[ ! -d $VIM_CONFIG_DIR/colors ]]
+then
+	mkdir -p "$VIM_CONFIG_DIR"/colors | tee -a "$SETUP_LOG"
+fi
+timestamp=$(date +%T.%N)
+echo -ne "\n\r$TOPIC --- Copy color scheme --- : $timestamp\n" | tee -a "$SETUP_LOG"
+cp "$SETUP_DIR"/configs/vim_theme/PaperColor.vim "$VIM_CONFIG_DIR"/colors/ | tee -a "$SETUP_LOG"
+
+if [[ ! -d $VIM_CONFIG_DIR/plugged ]]
+then
+	mkdir -p "$VIM_CONFIG_DIR"/plugged | tee -a "$SETUP_LOG"
+fi
+
+timestamp=$(date +%T.%N)
+echo -ne "\n\r$TOPIC --- Make VIM system editor --- : $timestamp\n" | tee -a "$SETUP_LOG"
+sudo update-alternatives --config editor | tee -a "$SETUP_LOG"
+
+timestamp=$(date +%T.%N)
+echo -ne "\n\r$TOPIC --- Intstallation complete --- : $timestamp\n" | tee -a "$SETUP_LOG"
+
+# End - Install VIM
+### }
+
+
+### {
+# Begin - Install TMUX
+
+TOPIC="TMUX:"
+TMUX_CONFIG_DIR="$HOME/.tmux"
+
+timestamp=$(date +%T.%N)
+echo -ne "$TOPIC --- Installing --- : $timestamp\n" | tee -a "$SETUP_LOG"
+sudo apt-get install -y tmux | tee -a "$SETUP_LOG"
+
+if [[ ! -d $TMUX_CONFIG_DIR/plugins ]]
+then
+    mkdir -p $TMUX_CONFIG_DIR/plugins 
+fi
+timestamp=$(date +%T.%N)
+echo -ne "$TOPIC --- TMUX plug --- : $timestamp\n" | tee -a "$SETUP_LOG"
+git clone https://github.com/tmux-plugins/tpm "$TMUX_CONFIG_DIR"/plugins/tpm | tee -a "$SETUP_LOG"
+
+timestamp=$(date +%T.%N)
+echo -ne "\n\r$TOPIC --- Copy .tmux.conf --- : $timestamp\n" | tee -a "$SETUP_LOG"
+cp ~/.tmux.conf ~/.tmux.conf.back | tee -a "$SETUP_LOG"
+cp "$SETUP_DIR"/configs/tmux/.tmux.conf "$HOME"/ | tee -a "$SETUP_LOG"
+
+timestamp=$(date +%T.%N)
+echo -ne "\n\r$TOPIC --- Intstallation complete --- : $timestamp\n" | tee -a "$SETUP_LOG"
+
+# End - Install TMUX
+### }
+
+
+### {
+# Begin - Install powerline for bash
+
+TOPIC="Powerline:"
+POWERLINE_CONFIG_DIR="$HOME/.config/powerline"
+timestamp=$(date +%T.%N)
+
+echo -ne "$TOPIC --- Installing --- : $timestamp\n" | tee -a "$SETUP_LOG"
+sudo apt-get install -y powerline | tee -a "$SETUP_LOG"
+
+if [[ ! -d $POWERLINE_CONFIG_DIR ]]
+then
+    mkdir -p $POWERLINE_CONFIG_DIR
+fi
+timestamp=$(date +%T.%N)
+echo -ne "$TOPIC --- Copy configs --- : $timestamp\n" | tee -a "$SETUP_LOG"
+cp -r "$SETUP_DIR"/configs/powerline/* "$POWERLINE_CONFIG_DIR"/ | tee -a "$SETUP_LOG"
+
+timestamp=$(date +%T.%N)
+echo -ne "$TOPIC --- Installation complete --- : $timestamp\n" | tee -a "$SETUP_LOG"
+
+# End - Install powerline for bash
+### }
+
+
+### {
+# Begin - Setup Bash environment
+
+TOPIC="Bash environment:"
+timestamp=$(date +%T.%N)
+echo -ne "$TOPIC --- creating my .bashrc file --- : $timestamp\n" | tee -a "$SETUP_LOG"
+cp ~/.bashrc ~/.bashrc_copy | tee -a "$SETUP_LOG"
+sed -i_back 's/\(shopt -s histappend\|^HIST*\)/# &/' ~/.bashrc | tee -a "$SETUP_LOG"
+cat "$SETUP_DIR"/configs/.bash_mine ~/.bashrc_copy > ~/.bashrc | tee -a "$SETUP_LOG"
+rm -f ~/.bashr_copy | tee -a "$SETUP_LOG"
+cp "$SETUP_DIR"/configs/.inputrc "$HOME"/ | tee -a "$SETUP_LOG"
+echo -ne "$TOPIC --- Bash setup is complete --- : $timestamp\n" | tee -a "$SETUP_LOG"
+
+# End - Setup Bash environment
+### }
+
+source ~/.bashrc
